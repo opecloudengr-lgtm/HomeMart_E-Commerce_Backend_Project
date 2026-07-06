a967bb4098badf07d9197c1d6ae24843395d66ae
opecloudengr-lgtm <ope.cloudengr@gmail.com>
2026-07-06 14:47:14 +0100

    added instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
 # Application factory
+from flask import Flask
+from .config import Config
+from .extensions import db, migrate, ma
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    ma.init_app(app)
+
+    @app.route('/')
+    def index():
+        return {"success": True, "message": "Welcome to the HomeMart E-Commerce Backend!"}
+    return app
