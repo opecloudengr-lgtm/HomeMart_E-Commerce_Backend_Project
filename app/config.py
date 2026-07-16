82269723a6b793288c60b8cedd8b2ed8d6f37963
opecloudengr-lgtm <ope.cloudengr@gmail.com>
2026-07-16 15:38:43 +0100

    updated config.py

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
 
 load_dotenv()
 
@@ -8,5 +9,7 @@ BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY')
     JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=15)
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=30)
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', f"sqlite:///{os.path.join(BASE_DIR, 'homemart.db')}")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
